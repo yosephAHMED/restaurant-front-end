3d15a712be4601da2727d83acc07bd5677bedc83
YosephAhmed <yosephahmed@aol.com>
2020-06-18 14:10:53 -0400

    Added some more styling to Services component.

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,9 +4,21 @@ import { SectionTitle } from "./";
 export default class Services extends Component {
   render() {
     return (
+      <>
         <div>
-            <SectionTitle title="A tavola non si invecchia" quote='"At the table, you dont get old"' subtitle="Located in Staten Island, New York City" />
+          <SectionTitle
+            title="A tavola non si invecchia"
+            quote='"At the table, you dont get old"'
+            subtitle="Located in Staten Island, New York City"
+          />
         </div>
-    )
+        <div className="about_us">
+            helooooooooooooooo
+        </div>
+        <div className="about_us_picture">
+            picutre
+        </div>
+      </>
+    );
   }
 }
