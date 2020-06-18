17b642e809db3aefa7828bf3007490909fc30b46
YosephAhmed <yosephahmed@aol.com>
2020-06-18 11:40:16 -0400

    Added an optional quote prop to the SectionTitle component

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function Title({ title, subtitle }) {
+export default function Title({ title, quote, subtitle }) {
   return (
     <div className="section-title">
       <h4>{title}</h4>
+      <h6>{quote}</h6>
       <p>{subtitle}</p>
       <div />
     </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -5,7 +5,7 @@ export default class Services extends Component {
   render() {
     return (
         <div>
-            <SectionTitle title="A tavola non si invecchia" subtitle="Located in Staten Island, New York City" />
+            <SectionTitle title="A tavola non si invecchia" quote='"At the table, you dont get old"' subtitle="Located in Staten Island, New York City" />
         </div>
     )
   }
