ee3cd3ad68077213aa9f720805157fe6041ba35c
YosephAhmed <yosephahmed@aol.com>
2020-06-18 15:40:03 -0400

    Finished the About component and added styling. About now contains two redirects. One to location (which is not yet implemented) and one to gallery (which is also not implemented yet). For now, these will just redirect to the Home page.

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { SectionTitle } from ".";
+import { BsImages } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 export default class About extends Component {
   render() {
@@ -13,10 +15,26 @@ export default class About extends Component {
           />
         </div>
         <div className="about_us">
-            helooooooooooooooo
-        </div>
-        <div className="about_us_picture">
-            picutre
+          <div className="about_us_content">
+            <header>
+              <h4>About us</h4>
+              <div/>
+            </header>
+            <p>
+              <a href="/location-hours/">Located </a>
+              within the heart of Staten Island, the newly renovated Delish
+              Dream Dish restaurant offers a collection of culinary experiences
+              that will deliver authentic Italian cuisine made by highly
+              experienced Italian chefs. Our menu is updated seasonly so that we
+              can always deliver the best ingredients to your plate. We promise
+              to create experiences that feel like home - come eat with us.
+            </p>
+          </div>
+          <div className="about_us_picture">
+            <Link to="/">
+              <BsImages className="circle-icon" />
+            </Link>
+          </div>
         </div>
       </>
     );
