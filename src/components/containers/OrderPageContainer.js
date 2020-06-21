14366fc8d163519d624ff8f6555cebfaed66b305
YosephAhmed <yosephahmed@aol.com>
2020-06-21 12:00:28 -0400

    Adjusted code for OrderPage axios calls. Seperated each category of food into its own array. This should make displaying the results in their respective div sections a lot easier.

--- src/components/containers/OrderPageContainer.js
+++ src/components/containers/OrderPageContainer.js
@@ -10,38 +10,24 @@ class OrderPageContainer extends Component {
   constructor() {
     super();
     this.state = {
-      fooditems: [],
+      appetizersArr: [],
+      entreesArr: [],
+      dessertArr: [],
+      drinks: [],
       order: [],
     };
   }
 
   componentDidMount() {
-    let appetizers = "http://localhost:3001/menu/appetizer";
-    let entrees = "http://localhost:3001/menu/entree";
-    let dessert = "http://localhost:3001/menu/dessert";
-    let drinks = "http://localhost:3001/menu/drinks";
+    let appetizersLink = "http://localhost:3001/menu/appetizer";
+    let entreesLink = "http://localhost:3001/menu/entree";
+    let dessertLink = "http://localhost:3001/menu/dessert";
+    let drinksLink = "http://localhost:3001/menu/drinks";
 
-    const requestAppetizers = axios.get(appetizers);
-    const requestEntrees = axios.get(entrees);
-    const requestDessert = axios.get(dessert);
-    const requestDrinks = axios.get(drinks);
-
-    // axios
-    //   .get("http://localhost:3001/menu")
-    //   .then((res) => {
-    //     let data = res.data;
-    //     const arr = [];
-
-    //     data.forEach((element) => {
-    //       arr.push(element);
-    //     });
-    //     this.setState({ fooditems: arr });
-
-    //     console.log("ARRAY: " + this.state.fooditems);
-    //   })
-    //   .catch((err) => {
-    //     console.log("Error: " + err);
-    //   });
+    const requestAppetizers = axios.get(appetizersLink);
+    const requestEntrees = axios.get(entreesLink);
+    const requestDessert = axios.get(dessertLink);
+    const requestDrinks = axios.get(drinksLink);
 
     axios.all([requestAppetizers, requestEntrees, requestDessert, requestDrinks])
     .then(axios.spread((...res) => {
@@ -50,21 +36,29 @@ class OrderPageContainer extends Component {
       const responseDessert = res[2];
       const responseDrinks = res[3];
 
+      console.log("data in res: ", res);
+      console.log("data in responseAppetizers: ", responseAppetizers);
+      console.log("data in responseEntrees: ", responseEntrees);
+      console.log("data in responseDessert: ", responseDessert);
+      console.log("data in responseDrinks: ", responseDrinks);
+
       // do something with the data inside of responses
         // each response should be an array that contains an object for each food
         // that has the corresponding category we were searching for
         // so if we push each response array to our state variable: fooditems
         // we would have an array containing arrays that contain objects
 
-      let menuArr = [];
-      menuArr.push (
-        responseAppetizers, responseEntrees, responseDessert, responseDrinks
-      );
+      this.setState({ 
+        appetizers: responseAppetizers,
+        entrees: responseEntrees,
+        dessert: responseDessert,
+        drinks: responseDrinks,
+      });
 
-      this.setState({ fooditems: menuArr });
-
-      console.log("array containing arrays: " + this.state.fooditems);
-
+      console.log("appetizers state: ", this.state.appetizers);
+      console.log("entrees state: ", this.state.entrees);
+      console.log("desserts state: ", this.state.desserts);
+      console.log("drinks state: ", this.state.drinks);
 
     })).catch(err => {
       console.log("Error: " + err);
@@ -72,7 +66,12 @@ class OrderPageContainer extends Component {
   }
 
   render() {
-    const { fooditems } = this.state;
+    const { 
+      appetizers,
+      entrees,
+      desserts,
+      drinks, 
+    } = this.state;
     return (
       <>
         <Hero hero="orderHero">
@@ -86,12 +85,13 @@ class OrderPageContainer extends Component {
           </Banner>
         </Hero>
 
-        {fooditems.map((value, k) => (
+        {/* {appetizers.data.map((value, k) => (
           <h4 key={k}>
             {" "}
             {value.fooditem}&nbsp;{value.fooddescription}&nbsp;{value.foodprice}
           </h4>
-        ))}
+        ))} */}
+
       </>
     );
   }
