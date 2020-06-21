b8c6df71f01c4e5c8b0a63b96dcf3da364f22c94
YosephAhmed <yosephahmed@aol.com>
2020-06-21 16:16:57 -0400

    Added styling to the OrderPageContainer and added the correct method to printing out the information for each food in its respective array category. The menu will take up the LHS of the screen (50%), whereas the order section for the user will take up the RHS of the screen (50%). On the LHS, the menu is further divied into two equal columns, such that for each row, 2 items are able to display correctly.

--- src/components/containers/OrderPageContainer.js
+++ src/components/containers/OrderPageContainer.js
@@ -29,76 +29,163 @@ class OrderPageContainer extends Component {
     const requestDessert = axios.get(dessertLink);
     const requestDrinks = axios.get(drinksLink);
 
-    axios.all([requestAppetizers, requestEntrees, requestDessert, requestDrinks])
-    .then(axios.spread((...res) => {
-      const responseAppetizers = res[0];
-      const responseEntrees = res[1];
-      const responseDessert = res[2];
-      const responseDrinks = res[3];
+    axios
+      .all([requestAppetizers, requestEntrees, requestDessert, requestDrinks])
+      .then(
+        axios.spread((...res) => {
+          const responseAppetizers = res[0];
+          const responseEntrees = res[1];
+          const responseDessert = res[2];
+          const responseDrinks = res[3];
 
-      // console.log("data in res: ", res);
-      console.log("data in responseAppetizers: ", responseAppetizers.data);
-      // console.log("data in responseEntrees: ", responseEntrees);
-      // console.log("data in responseDessert: ", responseDessert);
-      // console.log("data in responseDrinks: ", responseDrinks);
+          console.log("data in responseAppetizers: ", responseAppetizers.data);
 
-      // do something with the data inside of responses
-        // each response should be an array that contains an object for each food
-        // that has the corresponding category we were searching for
-        // so if we push each response array to our state variable: fooditems
-        // we would have an array containing arrays that contain objects
+          // do something with the data inside of responses
+          // each response should be an array that contains an object for each food
+          // that has the corresponding category we were searching for
+          // so if we push each response array to our state variable: fooditems
+          // we would have an array containing arrays that contain objects
 
-      this.setState({ 
-        appetizersArr: responseAppetizers.data,
-        entreesArr: responseEntrees.data,
-        dessertArr: responseDessert.data,
-        drinksArr: responseDrinks.data,
+          this.setState({
+            appetizersArr: responseAppetizers.data,
+            entreesArr: responseEntrees.data,
+            dessertArr: responseDessert.data,
+            drinksArr: responseDrinks.data,
+          });
+
+          // console.log("appetizers state: ", this.state.appetizersArr);
+          // console.log("entrees state: ", this.state.entrees);
+          // console.log("desserts state: ", this.state.desserts);
+          // console.log("drinks state: ", this.state.drinks);
+        })
+      )
+      .catch((err) => {
+        console.log("Error: " + err);
       });
-
-      console.log("appetizers state: ", this.state.appetizersArr);
-      console.log("entrees state: ", this.state.entrees);
-      console.log("desserts state: ", this.state.desserts);
-      console.log("drinks state: ", this.state.drinks);
-
-    })).catch(err => {
-      console.log("Error: " + err);
-    });
   }
 
   render() {
-    const { 
-      appetizersArr,
-      entreesArr,
-      dessertsArr,
-      drinksArrArr, 
-    } = this.state;
+    const { appetizersArr, entreesArr, dessertsArr, drinksArr } = this.state;
     return (
       <>
         <Hero hero="orderHero">
           <Banner
             title="Order Now"
-            subtitle="Simply select the food you want then checkout"
-          >
-            <Link to="/" className="btn-primary">
-              Return Home
-            </Link>
-          </Banner>
+            subtitle="Authentic Italian Menu in New York"
+          />
         </Hero>
-        {console.log("appetizers in render: ", appetizersArr)}
-        {appetizersArr.map((value, k) => (
-          <h4 key={k}>
-            {" "}
-            {value.fooditem}&nbsp;{value.fooddescription}&nbsp;{value.foodprice}
-          </h4>
-        ))}
 
-        {entreesArr.map((value, k) => (
-          <h4 key={k}>
-            {" "}
-            {value.fooditem}&nbsp;{value.fooddescription}&nbsp;{value.foodprice}
-          </h4>
-        ))}
+        {/* {console.log("appetizers in render: ", appetizersArr)} */}
 
+        <div className="menu-order-container">
+
+          {/* LHS for MENU */}
+          <div className="menu-main-container-lhs">
+
+            {/* APPETIZERS AND SALADS SECTION */}
+            <div className="menu-section-container">
+              <span className="menu-section-container-name">
+                APPETIZERS AND SALADS
+              </span>
+            </div>
+            <div className="menu-sub-section">
+                 {/* PRINT OUT APPETIZERS ARRAY */}
+                {appetizersArr.map((food, item) => (
+                  <div className="menu-sub-section-item">
+                    <div className="menu-sub-section-item-name">
+                      {food.fooditem}
+                    </div>
+                    <div className="menu-sub-section-item-price">
+                      ${food.foodprice}
+                    </div>
+                    <div className="menu-sub-section-item-description">
+                      {food.fooddescription}
+                    </div>
+                  </div>
+                ))}
+                {/* END OF APPETIZER ARRAY SECTION */}
+            </div>
+            {/* END OF APPETIZERS AND SALADS SECTION */}
+
+            {/* ENTREES SECTION */}
+            <div className="menu-section-container">
+              <span className="menu-section-container-name">
+                ENTREES
+              </span>
+            </div>
+            <div className="menu-sub-section">
+                 {/* PRINT OUT ENTREES ARRAY */}
+                {entreesArr.map((food, item) => (
+                  <div className="menu-sub-section-item">
+                    <div className="menu-sub-section-item-name">
+                      {food.fooditem}
+                    </div>
+                    <div className="menu-sub-section-item-price">
+                      ${food.foodprice}
+                    </div>
+                    <div className="menu-sub-section-item-description">
+                      {food.fooddescription}
+                    </div>
+                  </div>
+                ))}
+                {/* END OF ENTREES ARRAY */}
+            </div>
+            {/* END OF ENTREES SECTION */}
+
+            {/* DESSERTS SECTION */}
+            <div className="menu-section-container">
+              <span className="menu-section-container-name">
+                DESSERTS
+              </span>
+            </div>
+            <div className="menu-sub-section">
+                 {/* PRINT OUT DESSERTS ARRAY */}
+                {dessertsArr.map((food, item) => (
+                  <div className="menu-sub-section-item">
+                    <div className="menu-sub-section-item-name">
+                      {food.fooditem}
+                    </div>
+                    <div className="menu-sub-section-item-price">
+                      ${food.foodprice}
+                    </div>
+                    <div className="menu-sub-section-item-description">
+                      {food.fooddescription}
+                    </div>
+                  </div>
+                ))}
+                {/* END OF DESSERTS ARRAY */}
+            </div>
+            {/* END OF DESSERTS SECTION */}
+
+            {/* DRINKS SECTION */}
+            <div className="menu-section-container">
+              <span className="menu-section-container-name">
+                DRINKS
+              </span>
+            </div>
+            <div className="menu-sub-section">
+                 {/* PRINT OUT DRINKS ARRAY */}
+                {drinksArr.map((food, item) => (
+                  <div className="menu-sub-section-item">
+                    <div className="menu-sub-section-item-name">
+                      {food.fooditem}
+                    </div>
+                    <div className="menu-sub-section-item-price">
+                      ${food.foodprice}
+                    </div>
+                    <div className="menu-sub-section-item-description">
+                      {food.fooddescription}
+                    </div>
+                  </div>
+                ))}
+                {/* END OF DRINKS ARRAY */}
+            </div>
+            {/* END OF DRINKS SECTION */}
+
+          </div>
+          {/* RHS for ORDER */}
+          <div className="order-main-container-rhs">test</div>
+        </div>
       </>
     );
   }
